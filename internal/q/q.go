ebb460d0d2c6bd862df7da7a9bc90d646eab089c
Ahmad Anvari <ahmad.anv@gmail.com>
2025-03-24 13:55:42 +0330

    feat: implement access log

--- internal/q/q.go
+++ internal/q/q.go
@@ -1,12 +1,8 @@
 package q
 
-import (
-	"github.com/futureq-io/futureq/internal/storage"
-)
-
 type Q interface {
 	Connect() error
-	Consume(storage storage.TaskStorage) error
+	Consume() error
 	Publish(payload []byte)
 	Close()
 }
